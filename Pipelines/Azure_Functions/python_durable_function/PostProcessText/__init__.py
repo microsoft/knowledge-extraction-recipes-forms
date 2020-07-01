ad8a659a6397e72d82c9e2357189f796b580edb2
Mick Vleeshouwer <mick@imick.nl>
2020-07-01 13:05:47 +0200

    Remove TODO for now

--- Pipelines/Azure_Functions/python_durable_function/PostProcessText/__init__.py
+++ Pipelines/Azure_Functions/python_durable_function/PostProcessText/__init__.py
@@ -14,23 +14,6 @@ def main(result) -> str:
 
     result = json.loads(result)
 
-    # TODO Utilize typed RecognizedForm object
-    # for recognized_form in result:
-    #     recognized_form = RecognizedForm(
-    #         page_range=recognized_form.get("page_range"),
-    #         fields=recognized_form.get("fields"),
-    #         form_type=recognized_form.get("form_type"),
-    #         pages=recognized_form.get("pages"),
-    #     )
-
-    #     print("Form type ID: {}".format(recognized_form.form_type))
-    #     for label, field in recognized_form.fields.items():
-    #         print(
-    #             "Field '{}' has value '{}' with a confidence score of {}".format(
-    #                 label, field.value, field.confidence
-    #             )
-    #         )
-
     # Normalize text etc.
     processed_result = []
 
