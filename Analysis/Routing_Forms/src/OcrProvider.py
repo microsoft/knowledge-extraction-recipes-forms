05559048879b3472c40417aedf510e6a499d2870
Jon Malsan <jomalsan@microsoft.com>
2021-03-11 13:56:04 -0800

    Add liscense to new files

--- Analysis/Routing_Forms/src/OcrProvider.py
+++ Analysis/Routing_Forms/src/OcrProvider.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python
+
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
 from abc import ABC, abstractmethod
 from typing import Dict, List
 
