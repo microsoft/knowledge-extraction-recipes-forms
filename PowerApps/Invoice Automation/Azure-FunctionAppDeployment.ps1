3c6399fbe0648a53c558c5559f8891313b8c7cdb
Steve Pucelik <steve@pucelik.net>
2021-05-24 19:24:55 -0400

    Updated PS References

--- PowerApps/Invoice Automation/Azure-FunctionAppDeployment.ps1
+++ PowerApps/Invoice Automation/Azure-FunctionAppDeployment.ps1
@@ -1,33 +1,13 @@
-#http://www.redbaronofazure.com/?cat=428
-#https://demiliani.com/2020/03/26/a-quick-way-to-deploy-your-azure-functions-in-the-cloud/
-
 
 $location = "East US"
 $resourceGroupName = "InvoiceAutomation"
 $storageAccountName = "invoicenautomationrg"
 $functionName = "InvoiceLineItems"
-$sourceZipPath = "C:\Users\stpuceli\source\repos\HelloWorld\bin\Release\netcoreapp3.0\publish\InvoiceLineItems.zip"
-
-#Install-Module AzureRM -AllowClobber
-#Import-Module AzureRM
-
-#Then issue command:
-
+$sourceZipPath = "<SourceZipPath>\InvoiceLineItems.zip"
 
 Connect-AzAccount
 
-#New-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName -Location $location -SkuName "Standard_LRS"
-
-
-#New-AzFunctionApp -Name $functionName -StorageAccountName $storageAccountName -Location $location -ResourceGroupName $resourceGroupName -FunctionsVersion 2 -Runtime DotNet
+New-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName -Location $location -SkuName "Standard_LRS"
+New-AzFunctionApp -Name $functionName -StorageAccountName $storageAccountName -Location $location -ResourceGroupName $resourceGroupName -FunctionsVersion 2 -Runtime DotNet
 
 Publish-AzWebapp -ResourceGroupName $resourceGroupName -Name $functionName -ArchivePath $sourceZipPath
-
-New-AzureRmResource -ResourceGroupName $resourceGroupName -ResourceType "Microsoft.Web/Sites" -ResourceName $functionName -kind "functionapp" -Location $location -Properties @{} -force
-
-
-az functionapp create --name $functionName --storage-account $storageAccountName --consumption-plan-location "East US" --resource-group $resourceGroupName --functions-version 2
-
-
-
-az webapp deployment source config-zip -g $resourceGroupName -n $functionName --src $sourceZipPath
